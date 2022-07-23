6688f041318206b587212339e86f05d11eab9e79
Jakub Stepnik <jakubstepni1k@gmail.com>
2022-07-23 14:53:37 +0200

    debugin active button

--- active-button.js
+++ active-button.js
@@ -1,23 +1,59 @@
 const f_button = document.getElementById("f_button");
 const g_button = document.getElementById("g_button");
 
-let buttonBool= true;
+function click_button_f(){
+    
+    const textNodeArray = ["iloczny rozłaczne","rzędy elementów",
+    "fg","znak permutacji"]
 
-function click_button(){
-    //f_button.style.background = "green";
-    //f_button.style.color = "red";
-    const textNodeArray = ["iloczny rozłaczne","rzędy elementów","fg","znak permutacji"]
-    if(buttonBool){
+    if(!f_button.classList.contains("activ")){
         f_button.classList.add("activ");
         for(let i =0; i<textNodeArray.length; i++){
             const createButton = document.createElement("button");
             const textNode = document.createTextNode(textNodeArray[i]);
             createButton.appendChild(textNode);
+            createButton.setAttribute("id","buttonF"+i);
             document.getElementById("do_button").appendChild(createButton);
+            g_button.classList.remove("activ");
         }
+    }else {
+        let removeButton;
+        for(let i=0; i<textNodeArray.length; i++){
+            removeButton = document.getElementById("buttonF"+i);
+            console.log(removeButton);
+            removeButton.parentNode.removeChild(removeButton);
+        }
+        f_button.classList.remove("activ");
+        g_button.classList.remove("activ");
     }
-    buttonBool = false;
-
 }
 
-f_button.addEventListener("click",click_button);
+
+function click_button_g(){
+    
+    const textNodeArray = ["iloczny rozłaczne","rzędy elementów",
+    "gf","znak permutacji"]
+
+    if(!g_button.classList.contains("activ")){
+        g_button.classList.add("activ");
+        for(let i =0; i<textNodeArray.length; i++){
+            const createButton = document.createElement("button");
+            const textNode = document.createTextNode(textNodeArray[i]);
+            createButton.appendChild(textNode);
+            createButton.setAttribute("id","buttonG"+i);
+            document.getElementById("do_button").appendChild(createButton);
+            f_button.classList.remove("activ");
+        }
+    }else {
+        let removeButton;
+        for(let i=0; i<textNodeArray.length; i++){
+            removeButton = document.getElementById("buttonG"+i);
+            console.log(removeButton);
+            removeButton.parentNode.removeChild(removeButton);
+        }
+        g_button.classList.remove("activ");
+        f_button.classList.remove("activ");
+    }
+}
+f_button.addEventListener("click",click_button_f);
+g_button.addEventListener("click",click_button_g);
