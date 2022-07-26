38557e02ad3f123d28222d9077ff9204ba2eeb12
Jakub Stepnik <jakubstepni1k@gmail.com>
2022-07-26 12:29:04 +0200

    obliczanie iloczny rozlacznego do potegi 1

--- active-button.js
+++ active-button.js
@@ -78,6 +78,7 @@ function doButton(){
             const createIlocznynyRozlaczne = document.createElement("input");
             createIlocznynyRozlaczne.setAttribute("type","text");
             createIlocznynyRozlaczne.setAttribute("id","liczba");
+            createIlocznynyRozlaczne.setAttribute("placeholder","Potęga");
             document.getElementById("iloczyn_number").appendChild(createIlocznynyRozlaczne);
             const createButtonAccept = document.createElement("button");
             const textButton = document.createTextNode("Oblicz");
@@ -88,10 +89,71 @@ function doButton(){
             const obliczLiczbe = document.getElementById("obliczLiczbe");
             
             function obliczLiczbeFuncjca(){
-                const obliczLiczbeValue = document.getElementById("liczba").value;
-                console.log(obliczLiczbeValue);
+                const liczbaValue = document.getElementById("liczba").value;
+                let fArray = [];
+                for(let i = 1; i<11; i++){
+                    fArray.push(parseInt(document.getElementById("f"+i).value));
+                }
+                let iloczynyRozlaczneArray = [];
+                let saveNumber = 0;
+                if(liczbaValue === "1"){
+                    for(let i = 0; i<fArray.length;i++){
+                        let firstNumber = false;
+                        console.log(fArray[i]);
+                        for(let j = 1; j<=fArray.length; j++){
+                            if(fArray[i] === j){
+                                if(!firstNumber){
+                                    saveNumber = i+1;
+                                    console.log("saveNumber",saveNumber);
+                                    iloczynyRozlaczneArray.push(i+1);
+                                    firstNumber = true;
+                                }
+                                //console.log(i+1,fArray[i]);
+                                    //console.log(i+1,fArray[fArray[i]]);
+
+                                    i = fArray[i]-1;
+                                    j = 0;
+                                    iloczynyRozlaczneArray.push(i+1);
+                                    console.log(i+1);
+                                    //removefArray =  fArray.splice(i+1,1);
+                                    //console.log(removefArray);
+                                    if(i+1 === saveNumber){
+                                        console.log("end!!");
+                                        iloczynyRozlaczneArray.pop();
+                                        console.log(iloczynyRozlaczneArray);
+                                        // found index that wasn't use 
+                                        let nextIndex = 0;
+                                        for(let g = 0, count = 0; g<fArray.length; g++){
+                                            for(let k = 0; k<iloczynyRozlaczneArray.length;k++){
+                                                if(fArray[g] === iloczynyRozlaczneArray[k]){
+                                                    console.log(fArray[g],true);
+                                                    k = iloczynyRozlaczneArray;
+                                                    count = 0;
+                                                }else if(count < iloczynyRozlaczneArray.length){
+                                                    count++;
+                                                    nextIndex = g;
+                                                }
+                                            }
+                                        }
+                                        console.log(nextIndex);
+                                        i = nextIndex-1;
+                                        j = fArray.length+1;
+                                        if(iloczynyRozlaczneArray.length === fArray.length){
+                                            i = fArray.length;
+                                            j = fArray.length;
+                                        }
+                                    }
+                            }
+                        }
+                    }
+                }
+
+
+
+
             }
                 obliczLiczbe.addEventListener("click",obliczLiczbeFuncjca);
+                //obliczLiczbe.addEventListener("click",timer);
 
         }
         
